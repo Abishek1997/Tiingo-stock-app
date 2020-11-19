1ca3f7faf5bebb3082815264f3cd06ee317a5589
Abishek Narayanan <abishekn@usc.edu>
2020-11-19 00:51:18 -0800

    Setup and completed getCompanyDetails and onclick of Search submit

---
+++ app/src/main/java/com/example/tiingostock/network/pojos/CompanyDetailsResponse.java
@@ -0,0 +1,73 @@
+package com.example.tiingostock.network.pojos;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CompanyDetailsResponse {
+
+    private String exchangeCode;
+    private String description;
+    private String ticker;
+    private String startDate;
+    private String name;
+    private String endDate;
+    private final Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public String getExchangeCode() {
+        return exchangeCode;
+    }
+
+    public void setExchangeCode(String exchangeCode) {
+        this.exchangeCode = exchangeCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTicker() {
+        return ticker;
+    }
+
+    public void setTicker(String ticker) {
+        this.ticker = ticker;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
